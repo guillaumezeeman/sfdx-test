a3f1d72ed861c86f1c8dc95c66544d69345c2cde
Guillaume Zeeman <guillaume.zeeman@netive.nl>
2019-12-20 12:50:58 +0100

    Authenticate to hub

--- orgInit.sh
+++ orgInit.sh
@@ -3,7 +3,7 @@ CONSUMERKEY="$(heroku config:get CONSUMERKEY -a ntv-scratch-org-manager)"
 HUB_USERNAME="$(heroku config:get HUB_USERNAME -a ntv-scratch-org-manager)"
 
 # Create JWTKEY file
-heroku config:get JWTKEY -a ntv-scratch-org-manager > /app/tmp/server.key
+heroku config:get JWTKEY -a ntv-scratch-org-manager > /tmp/server.key
 
-sfdx force:auth:jwt:grant --clientid $CONSUMERKEY --username $HUB_USERNAME --jwtkeyfile /app/tmp/server.key
+sfdx force:auth:jwt:grant --clientid $CONSUMERKEY --username $HUB_USERNAME --jwtkeyfile /tmp/server.key -d
 sfdx force:org:create -f config/project-scratch-def.json -d 30 -s
