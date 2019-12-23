57cc21e3f12fd751bbeb3ddd4d6d80faaf7e2686
Guillaume Zeeman <guillaume.zeeman@netive.nl>
2019-12-23 08:16:30 +0100

    Directly pass variables instead of declaring them first.

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,8 @@
 # Create JWTKEY file
 heroku config:get JWTKEY -a ntv-scratch-org-manager > /tmp/server.key
 
-sfdx force:auth:jwt:grant --clientid "$(heroku config:get CONSUMERKEY -a ntv-scratch-org-manager)" --username "$(heroku config:get HUB_USERNAME -a ntv-scratch-org-manager)"  --jwtkeyfile /tmp/server.key -a hubOrg -d
+sfdx force:auth:jwt:grant --clientid "$(heroku config:get CONSUMERKEY -a ntv-scratch-org-manager)" \
+	--username "$(heroku config:get HUB_USERNAME -a ntv-scratch-org-manager)" \
+	--jwtkeyfile /tmp/server.key -a hubOrg -d
+
 sfdx force:org:create -f config/project-scratch-def.json -d 30 -s --json
