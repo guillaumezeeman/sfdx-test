81d526039a1d06563f8a74ab506cc4e189a0114f
Guillaume Zeeman <guillaume.zeeman@netive.nl>
2019-12-31 15:56:18 +0100

    Generate password and open org

--- orgInit.sh
+++ orgInit.sh
@@ -2,5 +2,6 @@
 heroku config:get JWTKEY -a ntv-scratch-org-manager > /tmp/server.key
 
 sfdx force:auth:jwt:grant --clientid "$(heroku config:get CONSUMERKEY -a ntv-scratch-org-manager)" --username "$(heroku config:get HUB_USERNAME -a ntv-scratch-org-manager)" --jwtkeyfile /tmp/server.key -a hubOrg -d
-
 sfdx force:org:create -f config/project-scratch-def.json -d 30 -s --json
+sfdx force:user:password:generate
+sfdx force:org:open
