1afef265ebb3b48c497dc04f89df1d46a0363758
Guillaume Zeeman <guillaume.zeeman@netive.nl>
2019-12-31 15:30:16 +0100

    Add --json again

--- orgInit.sh
+++ orgInit.sh
@@ -3,4 +3,4 @@ heroku config:get JWTKEY -a ntv-scratch-org-manager > /tmp/server.key
 
 sfdx force:auth:jwt:grant --clientid "$(heroku config:get CONSUMERKEY -a ntv-scratch-org-manager)" --username "$(heroku config:get HUB_USERNAME -a ntv-scratch-org-manager)" --jwtkeyfile /tmp/server.key -a hubOrg -d
 
-sfdx force:org:create -f config/project-scratch-def.json -d 30 -s
+sfdx force:org:create -f config/project-scratch-def.json -d 30 -s --json
